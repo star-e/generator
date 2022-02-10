a041459ab83568aca18e453c5f0de3becd2da448
Zhou Zhenglong <zlzhou.sh@outlook.com>
2022-02-10 18:19:37 +0800

    tidy

--- Cocos/renderer/pipeline/Overload.h
+++ Cocos/renderer/pipeline/Overload.h
@@ -8,12 +8,12 @@ namespace cc {
 // https://stackoverflow.com/questions/50510122/stdvariant-with-overloaded-lambdas-alternative-with-msvc
 
 template <class... Ts>
-struct overloaded_t {};
+struct overloaded_t {}; // NOLINT
 
 template <class T0>
 struct overloaded_t<T0> : T0 {
     using T0::operator();
-    overloaded_t(T0 t0)
+    overloaded_t(T0 t0) // NOLINT
     : T0(std::move(t0)) {}
 };
 
@@ -31,8 +31,8 @@ overloaded_t<Ts...> overload(Ts... ts) {
 }
 
 template <class... Ts>
-struct vertex_overloaded : overloaded_t<Ts...> {
-    vertex_overloaded(Ts... ts)
+struct vertex_overloaded : overloaded_t<Ts...> { // NOLINT
+    vertex_overloaded(Ts... ts) // NOLINT
     : overloaded_t<Ts...>(std::move(ts)...) {}
     template <class T>
     auto operator()(T* ptr) {
@@ -41,7 +41,7 @@ struct vertex_overloaded : overloaded_t<Ts...> {
 };
 
 template <class GraphT, class... Ts>
-auto visit_vertex(typename GraphT::vertex_descriptor v, GraphT& g, Ts... args) {
+auto visit_vertex(typename GraphT::vertex_descriptor v, GraphT& g, Ts... args) { // NOLINT
     return cc::visit(vertex_overloaded<Ts...>{std::move(args)...}, value(v, g));
 }
 
