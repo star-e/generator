2650b8fc5ba88318fde6529673608f9df9a6d91a
Zhou Zhenglong <zlzhou.sh@outlook.com>
2022-02-10 18:15:58 +0800

    tidy

--- Cocos/renderer/pipeline/invoke.hpp
+++ Cocos/renderer/pipeline/invoke.hpp
@@ -23,14 +23,14 @@ namespace invoke_hpp
     namespace impl
     {
         template < typename... Args >
-        struct make_void {
+        struct make_void { // NOLINT
             using type = void;
         };
     }
 
     template < typename... Args >
     using void_t = typename impl::make_void<Args...>::type;
-}
+} // namespace invoke_hpp
 
 //
 // integer_sequence
@@ -39,7 +39,7 @@ namespace invoke_hpp
 namespace invoke_hpp
 {
     template < typename T, T... Ints >
-    struct integer_sequence {
+    struct integer_sequence { // NOLINT
         using value_type = T;
         static constexpr std::size_t size() noexcept { return sizeof...(Ints); }
     };
@@ -50,7 +50,7 @@ namespace invoke_hpp
     namespace impl
     {
         template < typename T, std::size_t N, T... Ints >
-        struct make_integer_sequence_impl
+        struct make_integer_sequence_impl // NOLINT
         : make_integer_sequence_impl<T, N - 1, N - 1, Ints...> {};
 
         template < typename T, T... Ints >
@@ -66,7 +66,7 @@ namespace invoke_hpp
 
     template < typename... Ts >
     using index_sequence_for = make_index_sequence<sizeof...(Ts)>;
-}
+} // namespace invoke_hpp
 
 //
 // is_reference_wrapper
@@ -77,7 +77,7 @@ namespace invoke_hpp
     namespace impl
     {
         template < typename T >
-        struct is_reference_wrapper_impl
+        struct is_reference_wrapper_impl // NOLINT
         : std::false_type {};
 
         template < typename U >
@@ -86,9 +86,9 @@ namespace invoke_hpp
     }
 
     template < typename T >
-    struct is_reference_wrapper
+    struct is_reference_wrapper // NOLINT
     : impl::is_reference_wrapper_impl<typename std::remove_cv<T>::type> {};
-}
+} // namespace invoke_hpp
 
 //
 // invoke
@@ -107,7 +107,7 @@ namespace invoke_hpp
             typename Base, typename F, typename Derived,
             typename std::enable_if<std::is_base_of<Base, typename std::decay<Derived>::type>::value, int>::type = 0
         >
-        constexpr auto invoke_member_object_impl(F Base::* f, Derived&& ref)
+        constexpr auto invoke_member_object_impl(F Base::* f, Derived&& ref) // NOLINT
         INVOKE_HPP_NOEXCEPT_DECLTYPE_RETURN(
             std::forward<Derived>(ref).*f)
 
@@ -116,7 +116,7 @@ namespace invoke_hpp
             typename Base, typename F, typename RefWrap,
             typename std::enable_if<is_reference_wrapper<typename std::decay<RefWrap>::type>::value, int>::type = 0
         >
-        constexpr auto invoke_member_object_impl(F Base::* f, RefWrap&& ref)
+        constexpr auto invoke_member_object_impl(F Base::* f, RefWrap&& ref) // NOLINT
         INVOKE_HPP_NOEXCEPT_DECLTYPE_RETURN(
             ref.get().*f)
 
@@ -128,7 +128,7 @@ namespace invoke_hpp
                 !is_reference_wrapper<typename std::decay<Pointer>::type>::value
             , int>::type = 0
         >
-        constexpr auto invoke_member_object_impl(F Base::* f, Pointer&& ptr)
+        constexpr auto invoke_member_object_impl(F Base::* f, Pointer&& ptr) // NOLINT
         INVOKE_HPP_NOEXCEPT_DECLTYPE_RETURN(
             (*std::forward<Pointer>(ptr)).*f)
 
@@ -141,7 +141,7 @@ namespace invoke_hpp
             typename Base, typename F, typename Derived, typename... Args,
             typename std::enable_if<std::is_base_of<Base, typename std::decay<Derived>::type>::value, int>::type = 0
         >
-        constexpr auto invoke_member_function_impl(F Base::* f, Derived&& ref, Args&&... args)
+        constexpr auto invoke_member_function_impl(F Base::* f, Derived&& ref, Args&&... args) // NOLINT
         INVOKE_HPP_NOEXCEPT_DECLTYPE_RETURN(
             (std::forward<Derived>(ref).*f)(std::forward<Args>(args)...))
 
@@ -150,7 +150,7 @@ namespace invoke_hpp
             typename Base, typename F, typename RefWrap, typename... Args,
             typename std::enable_if<is_reference_wrapper<typename std::decay<RefWrap>::type>::value, int>::type = 0
         >
-        constexpr auto invoke_member_function_impl(F Base::* f, RefWrap&& ref, Args&&... args)
+        constexpr auto invoke_member_function_impl(F Base::* f, RefWrap&& ref, Args&&... args) // NOLINT
         INVOKE_HPP_NOEXCEPT_DECLTYPE_RETURN(
             (ref.get().*f)(std::forward<Args>(args)...))
 
@@ -162,10 +162,10 @@ namespace invoke_hpp
                 !is_reference_wrapper<typename std::decay<Pointer>::type>::value
             , int>::type = 0
         >
-        constexpr auto invoke_member_function_impl(F Base::* f, Pointer&& ptr, Args&&... args)
+        constexpr auto invoke_member_function_impl(F Base::* f, Pointer&& ptr, Args&&... args) // NOLINT
         INVOKE_HPP_NOEXCEPT_DECLTYPE_RETURN(
             ((*std::forward<Pointer>(ptr)).*f)(std::forward<Args>(args)...))
-    }
+    } // namespace impl
 
     template
     <
@@ -193,7 +193,7 @@ namespace invoke_hpp
     constexpr auto invoke(F&& f, Args&&... args)
     INVOKE_HPP_NOEXCEPT_DECLTYPE_RETURN(
         impl::invoke_member_function_impl(std::forward<F>(f), std::forward<Args>(args)...))
-}
+} // namespace invoke_hpp
 
 //
 // invoke_result
@@ -203,24 +203,24 @@ namespace invoke_hpp
 {
     namespace impl
     {
-        struct invoke_result_impl_tag {};
+        struct invoke_result_impl_tag {}; // NOLINT
 
         template < typename Void, typename F, typename... Args >
-        struct invoke_result_impl {};
+        struct invoke_result_impl {}; // NOLINT
 
         template < typename F, typename... Args >
         struct invoke_result_impl<void_t<invoke_result_impl_tag, decltype(invoke_hpp::invoke(std::declval<F>(), std::declval<Args>()...))>, F, Args...> {
             using type = decltype(invoke_hpp::invoke(std::declval<F>(), std::declval<Args>()...));
         };
-    }
+    } // namespace impl
 
     template < typename F, typename... Args >
-    struct invoke_result
+    struct invoke_result // NOLINT
     : impl::invoke_result_impl<void, F, Args...> {};
 
     template < typename F, typename... Args >
     using invoke_result_t = typename invoke_result<F, Args...>::type;
-}
+} // namespace invoke_hpp
 
 //
 // is_invocable
@@ -230,10 +230,10 @@ namespace invoke_hpp
 {
     namespace impl
     {
-        struct is_invocable_r_impl_tag {};
+        struct is_invocable_r_impl_tag {}; // NOLINT
 
         template < typename Void, typename R, typename F, typename... Args >
-        struct is_invocable_r_impl
+        struct is_invocable_r_impl // NOLINT
         : std::false_type {};
 
         template < typename R, typename F, typename... Args >
@@ -242,15 +242,15 @@ namespace invoke_hpp
             std::is_void<R>::value,
             std::true_type,
             std::is_convertible<invoke_result_t<F, Args...>, R>>::type {};
-    }
+    } // namespace impl
 
     template < typename R, typename F, typename... Args >
-    struct is_invocable_r
+    struct is_invocable_r // NOLINT
     : impl::is_invocable_r_impl<void, R, F, Args...> {};
 
     template < typename F, typename... Args >
     using is_invocable = is_invocable_r<void, F, Args...>;
-}
+} // namespace invoke_hpp
 
 //
 // apply
@@ -261,7 +261,7 @@ namespace invoke_hpp
     namespace impl
     {
         template < typename F, typename Tuple, std::size_t... I >
-        constexpr auto apply_impl(F&& f, Tuple&& args, index_sequence<I...>)
+        constexpr auto apply_impl(F&& f, Tuple&& args, index_sequence<I...>) // NOLINT
         INVOKE_HPP_NOEXCEPT_DECLTYPE_RETURN(
             invoke_hpp::invoke(
                 std::forward<F>(f),
@@ -275,6 +275,6 @@ namespace invoke_hpp
             std::forward<F>(f),
             std::forward<Tuple>(args),
             make_index_sequence<std::tuple_size<typename std::decay<Tuple>::type>::value>()))
-}
+} // namespace invoke_hpp
 
 #undef INVOKE_HPP_NOEXCEPT_DECLTYPE_RETURN
