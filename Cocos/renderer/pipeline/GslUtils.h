def764e5d182e93f21766752c9a3a07985ed7391
Zhou Zhenglong <zlzhou.sh@outlook.com>
2022-02-10 18:09:13 +0800

    tidy

--- Cocos/renderer/pipeline/GslUtils.h
+++ Cocos/renderer/pipeline/GslUtils.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <cocos/base/Macros.h>
+#include <utility>
 
 namespace cc {
 
@@ -7,14 +8,14 @@ namespace gsl {
 
 // narrow_cast(): a searchable way to do narrowing casts of values
 template <class T, class U>
-constexpr T narrow_cast(U&& u) noexcept {
+constexpr T narrow_cast(U &&u) noexcept { // NOLINT
     return static_cast<T>(std::forward<U>(u));
 }
 
-#define Expects(cond) CC_ASSERT(cond)
+#define Expects(cond) CC_ASSERT(cond) // NOLINT
 
-#define Ensures(cond) CC_ASSERT(cond)
+#define Ensures(cond) CC_ASSERT(cond) // NOLINT
 
-}
+} // namespace gsl
 
-}
+} // namespace cc
