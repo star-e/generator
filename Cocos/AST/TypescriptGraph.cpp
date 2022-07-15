f36b3d2242f61e7a2d94ae5204d883631df127e8
Zhou Zhenglong <zlzhou.sh@outlook.com>
2022-07-15 18:51:08 +0800

    setter added

--- Cocos/AST/TypescriptGraph.cpp
+++ Cocos/AST/TypescriptGraph.cpp
@@ -1640,19 +1640,21 @@ std::pmr::string generateGraph(const ModuleBuilder& builder,
                     }
                     OSS << "}\n";
 
-                    if (false && g.isTypescriptValueType(componentID)) {
-                        OSS << "set" << convertTag(c.mName) << " (v: " << vertexDescType
-                            << ", value: " << componentType << ") {\n";
-                        {
-                            INDENT();
-                            Expects(c.isValid());
-                            if (s.isVector()) {
-                                OSS << "this." << member << "[v] = value;\n";
-                            } else {
-                                OSS << "v." << member << " = value;\n";
+                    if (g.isTypescriptValueType(componentID)) {
+                        if (!(s.mNamedConcept.mComponent && s.mNamedConcept.mComponentName == c.mName)) {
+                            OSS << "set" << convertTag(c.mName) << " (v: " << vertexDescType
+                                << ", value: " << componentType << ") {\n";
+                            {
+                                INDENT();
+                                Expects(c.isValid());
+                                if (s.isVector()) {
+                                    OSS << "this." << member << "[v] = value;\n";
+                                } else {
+                                    OSS << "v." << member << " = value;\n";
+                                }
                             }
+                            OSS << "}\n";
                         }
-                        OSS << "}\n";
                     }
                 }
             }
