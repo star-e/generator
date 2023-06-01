d720d9913e9c73f5960239f38eaf2bfb2c7f4ce8
Zhou Zhenglong <zlzhou.sh@outlook.com>
2023-06-01 14:46:53 +0800

    enable subpass

--- Cocos/CocosBuilder/RenderInterface.cpp
+++ Cocos/CocosBuilder/RenderInterface.cpp
@@ -107,7 +107,7 @@ class RenderWindow;
 
 namespace render {
 
-constexpr bool ENABLE_SUBPASS = false;
+constexpr bool ENABLE_SUBPASS = true;
 constexpr bool ENABLE_GPU_DRIVEN = false;
 
 } // namespace render
