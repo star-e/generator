e4b5a232b377f43a0e4447198a8c3365ec3bd0fa
Zhou Zhenglong <zlzhou.sh@outlook.com>
2023-06-08 01:48:02 +0800

    ppl updated

--- Cocos/CocosBuilder/NativePipeline.cpp
+++ Cocos/CocosBuilder/NativePipeline.cpp
@@ -429,7 +429,7 @@ void buildRenderQueues(const RenderGraph& rg, const LayoutGraphData& lg, const p
 private:
 uint32_t getOrCreateSceneCullingQuery(const SceneData& sceneData);
 uint32_t createRenderQueue(SceneFlags sceneFlags, LayoutGraphData::vertex_descriptor subpassOrPassLayoutID);
-void collectCullingQueries(const RenderGraph& rg, const LayoutGraphData& lg, const pipeline::PipelineSceneData& pplSceneData);
+void collectCullingQueries(const RenderGraph& rg, const LayoutGraphData& lg);
 void batchCulling(const pipeline::PipelineSceneData& pplSceneData);
 void fillRenderQueues(const RenderGraph& rg, const pipeline::PipelineSceneData& pplSceneData);
 public:
