61e0d99be40d06110c1198eb13f64a804d5f8b30
Zhou Zhenglong <zlzhou.sh@outlook.com>
2023-08-30 11:43:26 +0800

    add SORT_BY_WORLD_BOUNDS_CENTER

--- Cocos/CocosBuilder/RenderCommon.cpp
+++ Cocos/CocosBuilder/RenderCommon.cpp
@@ -129,6 +129,7 @@ import { saveUniformBlock, loadUniformBlock } from './serialization';
                 (DRAW_NON_INSTANCING, 0x1000)
                 (REFLECTION_PROBE, 0x2000)
                 (GPU_DRIVEN, 0x4000)
+                (SORT_BY_WORLD_BOUNDS_CENTER, 0x8000)
                 (ALL, 0xFFFFFFFF)
             );
         }
